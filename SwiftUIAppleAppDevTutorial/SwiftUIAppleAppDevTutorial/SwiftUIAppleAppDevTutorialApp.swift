b559c2a2f997a5c0328dbc37eeeadc072d8c8844
Tin Jukić <tin.jukic@photomath.com>
2023-08-09 13:09:30 +0200

    Using stacks to arrange views
    Lesson 1

---
+++ SwiftUIAppleAppDevTutorial/SwiftUIAppleAppDevTutorial/SwiftUIAppleAppDevTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAppleAppDevTutorialApp.swift
+//  SwiftUIAppleAppDevTutorial
+//
+//  Created by Tin Jukic on 09.08.2023..
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAppleAppDevTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
